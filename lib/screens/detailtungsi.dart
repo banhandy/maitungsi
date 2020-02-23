64c3ed18401889a4811c6b4ac1c7d05badb7172b
Handy Ban <banhandy@gmail.com>
2020-02-23 14:03:09 +0700

    added detailed list screen

---
+++ lib/screens/detailtungsi.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailedListScreen extends StatefulWidget {
+  @override
+  _DetailedListScreenState createState() => _DetailedListScreenState();
+}
+
+class _DetailedListScreenState extends State<DetailedListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
