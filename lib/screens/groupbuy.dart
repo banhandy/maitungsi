5fae902b11d469836911456e1f10cb2dd0348bf8
Handy Ban <banhandy@gmail.com>
2020-02-24 20:47:54 +0700

    added category list on group buy screen

--- lib/screens/groupbuy.dart
+++ lib/screens/groupbuy.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class GroupBuy extends StatefulWidget {
   static String id = 'Group Buy';
+
   @override
   _GroupBuyState createState() => _GroupBuyState();
 }
@@ -15,24 +16,33 @@ class _GroupBuyState extends State<GroupBuy> {
   void initState() {
     super.initState();
     getCurrentUser();
-    categoryStream();
   }
 
-  void categoryStream() async {
-    await for (var snapshot
-        in Firestore.instance.collection('categories').snapshots()) {
-      for (var message in snapshot.documents) {
-        print(message.data);
-      }
-    }
+  void updateUI() {
+    categoryStream();
   }
 
   getCurrentUser() async {
     try {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       logInUser = prefs.getString('email');
+      setState(() {
+        updateUI();
+      });
     } catch (e) {
       print(e);
+    }
+  }
+
+  void categoryStream() async {
+    print(logInUser);
+    await for (var snapshot in Firestore.instance
+        .collection('categories')
+        .where("email", isEqualTo: logInUser)
+        .snapshots()) {
+      for (var message in snapshot.documents) {
+        print(message.data);
+      }
     }
   }
 
@@ -52,11 +62,49 @@ class _GroupBuyState extends State<GroupBuy> {
           )
         ],
       ),
-      body: Text('Welcome $logInUser'),
+      body: CategoryList(logInUser),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {},
       ),
     );
+  }
+}
+
+class CategoryList extends StatelessWidget {
+  final String logInUser;
+  CategoryList(this.logInUser);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance
+            .collection('categories')
+            .where("email", isEqualTo: logInUser)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.blueAccent,
+              ),
+            );
+          }
+          List<MaterialButton> categoryListWidget = [];
+          final categoryList = snapshot.data.documents.reversed;
+          for (var category in categoryList) {
+            final MaterialButton categoryButton = MaterialButton(
+              child: Text(category.data['category']),
+              onPressed: () {},
+            );
+            categoryListWidget.add(categoryButton);
+          }
+          return ListWheelScrollView(
+            children: categoryListWidget,
+            itemExtent: 40.0,
+            useMagnifier: true,
+            magnification: 1.2,
+          );
+        });
   }
 }
